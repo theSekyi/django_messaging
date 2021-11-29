eb6e37643d0d2182f6c419eb4c47769eed065356
theSekyi <emmanuelkwamesekyi@gmail.com>
2021-11-29 21:32:54 +0200

    added Base Model

--- django_messaging/direct_messaging/models.py
+++ django_messaging/direct_messaging/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class BaseModel(models.Model):
+
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True  # Set this model as Abstract
