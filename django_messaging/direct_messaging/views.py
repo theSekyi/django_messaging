37634027c37d640942622f74240ca164a0f5ba29
theSekyi <emmanuelkwamesekyi@gmail.com>
2021-12-04 23:41:42 +0200

    removed login_required

--- django_messaging/direct_messaging/views.py
+++ django_messaging/direct_messaging/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import MessageForm
-from django.contrib.auth.decorators import login_required
+
+# from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
