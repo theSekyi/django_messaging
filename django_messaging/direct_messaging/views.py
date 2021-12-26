8b7147f26a6b25d2a1989f404cc72a580f52f9db
theSekyi <emmanuelkwamesekyi@gmail.com>
2021-12-26 23:48:26 +0200

    added code for inbox

--- django_messaging/direct_messaging/views.py
+++ django_messaging/direct_messaging/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from .forms import MessageForm
+from .models import MessageModel
 
 # from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 def inbox(request):
-    pass
+    user = request.user
+    messages = MessageModel.get_messages(user=user)
+    active_direct = None
