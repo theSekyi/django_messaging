ca1199367159b2de7d915f2bc43835ee15ad57d4
theSekyi <emmanuelkwamesekyi@gmail.com>
2021-12-27 22:30:57 +0200

    added preliminary code to inbox view

--- django_messaging/direct_messaging/views.py
+++ django_messaging/direct_messaging/views.py
@@ -10,3 +10,14 @@ def inbox(request):
     user = request.user
     messages = MessageModel.get_messages(user=user)
     active_direct = None
+    directs = None
+    directs.update(is_read=True)
+
+    for message in messages:
+        if message["user"].username == active_direct:
+            message["unread"] = 0
+
+    context = {"directs": directs, "messages": messages, "active_direct": active_direct}
+
+    # template = loader.get_template("direct_messaginf/inbox.html")
+    # return HttpResponse(template.render(context, request))
