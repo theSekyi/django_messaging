28eb1d5b56f48dc7a838754b79c05f0a7d7dadff
theSekyi <emmanuelkwamesekyi@gmail.com>
2022-01-01 23:20:33 +0200

    added view to url file

--- django_messaging/direct_messaging/urls.py
+++ django_messaging/direct_messaging/urls.py
@@ -1,8 +1,9 @@
 from django.views.generic import TemplateView
 
 from django.urls import path
+from .views import inbox
 
 
 urlpatterns = [
-    path("inbox/", TemplateView.as_view(template_name="direct_messaging/inbox.html"), name="inbox"),
+    path("inbox/", inbox, name="inbox"),
 ]
