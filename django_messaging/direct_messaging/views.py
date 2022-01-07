95625ca34e1dc4992d08db6a670f5da8a720baa8
theSekyi <emmanuelkwamesekyi@gmail.com>
2022-01-07 22:53:45 +0200

    removed prints

--- django_messaging/direct_messaging/views.py
+++ django_messaging/direct_messaging/views.py
@@ -41,11 +41,9 @@ def inbox(request):
 def search_users_to_dm(request):
     query = request.GET.get("username")
     users_paginator = None
-    print(f"Queries {query}")
 
     if query:
         users = User.objects.filter(Q(username__icontains=query))
-        print(users)
 
         paginator = Paginator(users, 5)
         page_number = request.GET.get("page")
